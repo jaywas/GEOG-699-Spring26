7153cbf51135303e5110d1548d637273583bc737
Jay Wasdin <jaywas@tamu.edu>
2026-01-16 09:38:08 -0600

    fix

--- homework/week01/Untitled-1.py
+++
@@ -1,8 +0,0 @@
-
-print("Printing cumulative sum from 1-10:")
-total = 0
-for i in range(1, 11):
-    total += i
-    print("Sum of 1 to " + str(i) + " is: " + str(total))
-print("Done printing numbers.")
-
